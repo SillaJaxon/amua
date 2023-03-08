56ee2435eb6769bc08406f9747133707fcf4f457
Silla Jaxon <sillajaxon@gmail.com>
2023-03-08 22:53:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,26 +7,24 @@ def app():
     st.sidebar.header("Decisions")
     option = st.sidebar.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
 
-    # Create the main content area
-    st.write(f"You selected {option}!")
 
     # Add a chat input and buttons
-    st.subheader("Chat Input")
-    chat_input = st.text_input("Enter your message:")
     st.subheader("Buttons")
     button_grid = st.beta_container()
     with button_grid:
         col1, col2, col3 = st.beta_columns(3)
-        if col1.button("Button 1"):
-            chat_input = "You clicked Button 1!"
-        if col2.button("Button 2"):
-            chat_input = "You clicked Button 2!"
-        if col3.button("Button 3"):
-            chat_input = "You clicked Button 3!"
+        if col1.button("I want to apprach the new lady"):
+            chat_suggection = "I want to apprach the new lady"
+        if col2.button("I want to start a new community"):
+            chat_suggection = "I want to start a new community"
+        if col3.chat_suggection("I want to learn swimming"):
+            chat_input = "I want to learn swimming"
 
     # Display the chat input
     st.subheader("Output")
     st.write(chat_input)
+    st.subheader("What is your decision in one sentence")
+    chat_input = st.text_input("Enter your thought here:")
 
 # Run the Streamlit app
 if __name__ == "__main__":
