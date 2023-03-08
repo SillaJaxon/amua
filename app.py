22d2cda4f7282cecdf4a80873362a2c16292ae2c
Silla Jaxon <sillajaxon@gmail.com>
2023-03-08 22:40:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,16 @@
 import streamlit as st
-from transformers import pipeline
-
-# Define a function to generate a response from GPT-2 model
-def generate_response(prompt):
-    # Load the GPT-2 model
-    generator = pipeline('text-generation', model='gpt2')
-
-    # Generate a response to the prompt
-    response = generator(prompt, max_length=1000, num_return_sequences=1)[0]['generated_text']
-
-    return response
 
 # Define the Streamlit app
 def app():
     # Set the app's title
-    st.title("Chat with ChatGPT")
+    st.title("My Streamlit App")
 
     # Create a sidebar menu
     st.sidebar.header("Settings")
-    prompt = st.sidebar.text_input("Prompt", "Hello, how are you?")
-    submit_button = st.sidebar.button("Submit")
+    option = st.sidebar.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
 
-    # Create a chat window
-    st.markdown("---")
-    st.markdown("### Chat")
-
-    chat_history = []
-    if submit_button:
-        # Add the user's prompt to the chat history
-        chat_history.append(("User", prompt))
-
-        # Generate a response to the user's prompt
-        response = generate_response(prompt)
-
-        # Add the response to the chat history
-        chat_history.append(("ChatGPT", response))
-
-    # Display the chat history
-    for speaker, text in chat_history:
-        if speaker == "User":
-            st.write(f"You: {text}")
-        else:
-            st.write(f"ChatGPT: {text}")
+    # Create the main content area
+    st.write(f"You selected {option}!")
 
 # Run the Streamlit app
 if __name__ == "__main__":
