68b79b44a1317bf68e6611ab59d72207160e3f6f
Silla Jaxon <sillajaxon@gmail.com>
2023-03-09 07:45:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,33 @@
 import streamlit as st
-import sidebar
-import suggestions
 
-# Define the options for the buttons in the sidebar
-options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
+def get_bot_response(user_input):
+    # Here you can implement your chatbot logic to generate a response
+    return "I'm sorry, I don't understand. Can you please rephrase your question?"
 
 # Define the layout of the app
-st.set_page_config(page_title="My Decision Maker", page_icon=":dart:")
+st.set_page_config(page_title="Chatbot", page_icon=":speech_balloon:")
 
-# Define the content of the main page
-st.header("My Decision Maker")
-if not st.session_state.get("button_clicked", False):
-    st.subheader("Choose any option here to get you started ...")
-
-# Define the sidebar
-selected_option = sidebar.add_sidebar(options)
-
-# Define the grid of buttons with generic suggestions
-if not st.session_state.get("button_clicked", False):
-    suggestions.add_suggestions()
+# Define the content of the app
+st.title("Chatbot")
 
 # Define the text input and send button
 col1, col2 = st.beta_columns([4, 1])
 with col1:
-    if "selected_suggestion" in st.session_state:
-        text_input = st.text_input("Enter your decision here...", value=st.session_state.selected_suggestion)
-    else:
-        text_input = st.text_input("Enter your decision here...")
-with col2:
-    st.write("") # Add some space
-    st.write("") # Add some space
-    if st.button("Send"):
-        st.session_state["button_clicked"] = True
-        if "selected_suggestion" in st.session_state:
-            st.session_state.selected_suggestion = None
-            text_input = ""
-        else:
-            st.session_state.text_input = text_input
-            text_input = ""
+    user_input = st.text_input("You:", key="user_input")
+
+if st.button("Send", key="send_button"):
+    bot_response = get_bot_response(user_input)
+    with col1:
+        st.text_area("Bot:", value=bot_response, height=100, key="bot_response")
+        st.session_state.bot_response_history.append(bot_response)
+        st.session_state.user_input_history.append(user_input)
+
+if "bot_response_history" not in st.session_state:
+    st.session_state.bot_response_history = []
+    st.session_state.user_input_history = []
+
+if st.session_state.bot_response_history:
+    st.write("Conversation History:")
+    for i in range(len(st.session_state.bot_response_history)):
+        st.write("You: " + st.session_state.user_input_history[i])
+        st.write("Bot: " + st.session_state.bot_response_history[i])
