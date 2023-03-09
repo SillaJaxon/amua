0e2b88154dbcec745f476490e7b668d1b3757ca9
Silla Jaxon <sillajaxon@gmail.com>
2023-03-09 07:57:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,32 @@
-import streamlit as st
+# Define a function to get user input
+def get_input(prompt):
+    return input(prompt + '\n> ')
 
-def get_bot_response(user_input):
-    # Here you can implement your chatbot logic to generate a response
-    return "I'm sorry, I don't understand. Can you please rephrase your question?"
+# Get the decision from the user
+decision = get_input('What decision do you need to make?')
 
-# Define the layout of the app
-st.set_page_config(page_title="Chatbot", page_icon=":speech_balloon:")
+# Get a list of options from the user
+options = []
+while True:
+    option = get_input('Enter an option (or enter nothing to stop):')
+    if option == '':
+        break
+    options.append(option)
 
-# Define the content of the app
-st.title("Chatbot")
+# Get a list of reasons for each option from the user
+option_reasons = {}
+for option in options:
+    reasons = []
+    while True:
+        reason = get_input(f'Enter a reason for "{option}" (or enter nothing to stop):')
+        if reason == '':
+            break
+        reasons.append(reason)
+    option_reasons[option] = reasons
 
-if "bot_response_history" not in st.session_state:
-    st.session_state.bot_response_history = []
-    st.session_state.user_input_history = []
+# Count the number of reasons for each option
+option_counts = {option: len(reasons) for option, reasons in option_reasons.items()}
 
-if st.session_state.bot_response_history:
-    st.write("Conversation History:")
-    for i in range(len(st.session_state.bot_response_history)):
-        st.write("You: " + st.session_state.user_input_history[i])
-        st.write("Bot: " + st.session_state.bot_response_history[i])
-
-# Define the text input and send button
-col1, col2 = st.beta_columns([4, 1])
-with col1:
-    user_input = st.text_input("You:", key="user_input", on_change=True, value="")
-    if st.session_state.user_input != "":
-        st.session_state.bot_response_history.append(get_bot_response(st.session_state.user_input))
-        st.session_state.user_input_history.append(st.session_state.user_input)
-        st.session_state.user_input = ""
-
-if st.button("Send", key="send_button"):
-    if st.session_state.user_input != "":
-        st.session_state.bot_response_history.append(get_bot_response(st.session_state.user_input))
-        st.session_state.user_input_history.append(st.session_state.user_input)
-        st.session_state.user_input = ""
-
-    if st.session_state.bot_response_history:
-        st.write("Conversation History:")
-        for i in range(len(st.session_state.bot_response_history)):
-            st.write("You: " + st.session_state.user_input_history[i])
-            st.write("Bot: " + st.session_state.bot_response_history[i])
+# Print the option with the most reasons
+max_option = max(option_counts, key=option_counts.get)
+print(f'The recommended option is "{max_option}" with {option_counts[max_option]} reasons.')
