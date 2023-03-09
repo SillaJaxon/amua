ec350cfa127d7165076b310c5a3b2472993fbbe7
Silla Jaxon <sillajaxon@gmail.com>
2023-03-09 07:31:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,16 +22,14 @@ suggestions.add_suggestions()
 col1, col2 = st.beta_columns([4, 1])
 with col1:
     if "selected_suggestion" in st.session_state:
-        text_input = st.text_input("Enter your decision here...", value=st.session_state.selected_suggestion)
+        st.text_input("Enter your decision here...", value=st.session_state.selected_suggestion, key="chat-input")
     else:
-        text_input = st.text_input("Enter your decision here...")
+        text_input = st.text_input("Enter your decision here...", key="chat-input")
 with col2:
-    st.write("") # Add some space
-    st.write("") # Add some space
-    if st.button("Send"):
+    if st.button("", key="send-button"):
         if "selected_suggestion" in st.session_state:
             st.session_state.selected_suggestion = None
-            text_input = ""
         else:
             st.session_state.text_input = text_input
-            text_input = ""
+        text_input = ""
+st.write(":email:", unsafe_allow_html=True)
