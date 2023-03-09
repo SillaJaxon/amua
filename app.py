75c288bea4e4e0d10943d5c4875a484c961f859f
Silla Jaxon <sillajaxon@gmail.com>
2023-03-09 07:25:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,43 @@ options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
 st.set_page_config(page_title="My Decision Maker", page_icon=":dart:")
 
 # Define the content of the main page
-content = """
-    <div style='display:flex;justify-content:center;'>
-        <div style='width:75%;'>
-            <h1 style='text-align:center;'>My Decision Maker</h1>
-            <h2 style='text-align:center;'>Choose your fate...</h2>
-            """ + sidebar.add_sidebar(options) + suggestions.add_suggestions() + """
-            <br>
-            <div style='text-align:center;'>
-                """ + (st.text_input("Enter your decision here...", value=st.session_state.selected_suggestion) if "selected_suggestion" in st.session_state else st.text_input("Enter your decision here...")) + """
-                <br>
-                <br>
-                <button style='margin:auto;display:block;' type="submit" class="btn btn-primary">Send</button>
-            </div>
-        </div>
-    </div>
-"""
+st.header("My Decision Maker")
+st.subheader("Choose your fate...")
 
-st.markdown(content, unsafe_allow_html=True)
+# Define the sidebar
+selected_option = sidebar.add_sidebar(options)
+
+# Define the grid of buttons with generic suggestions
+suggestions.add_suggestions()
+
+# Define the text input and send button
+if "selected_suggestion" in st.session_state:
+    st.text_input("Enter your decision here...", value=st.session_state.selected_suggestion)
+else:
+    text_input = st.text_input("Enter your decision here...")
+
+if st.button("Send"):
+    if "selected_suggestion" in st.session_state:
+        st.session_state.selected_suggestion = None
+        text_input = ""
+    else:
+        st.session_state.text_input = text_input
+        text_input = ""
+
+# Center the content
+st.markdown(
+    """
+    <style>
+    .centered {
+        display: flex;
+        justify-content: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Wrap the content in a div with the "centered" class
+st.markdown('<div class="centered">', unsafe_allow_html=True)
+# Your content here
+st.markdown('</div>', unsafe_allow_html=True)
