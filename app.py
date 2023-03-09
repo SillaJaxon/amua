51fb364a78600c369c96580ee7ad8387b3b54b10
Silla Jaxon <sillajaxon@gmail.com>
2023-03-09 06:49:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,30 +14,50 @@ selected_option = st.sidebar.radio("", options)
 st.header("My Decision Maker")
 st.subheader("Choose your fate...")
 
-# Define the grid of buttons
+# Define the grid of buttons with generic suggestions
 col1, col2, col3 = st.beta_columns(3)
 with col1:
-    st.button("Button 1")
+    if st.button("Do it"):
+        st.session_state.selected_suggestion = "Do it"
 with col2:
-    st.button("Button 2")
+    if st.button("Don't do it"):
+        st.session_state.selected_suggestion = "Don't do it"
 with col3:
-    st.button("Button 3")
+    if st.button("Ask someone else"):
+        st.session_state.selected_suggestion = "Ask someone else"
 
 col4, col5, col6 = st.beta_columns(3)
 with col4:
-    st.button("Button 4")
+    if st.button("Yes"):
+        st.session_state.selected_suggestion = "Yes"
 with col5:
-    st.button("Button 5")
+    if st.button("No"):
+        st.session_state.selected_suggestion = "No"
 with col6:
-    st.button("Button 6")
+    if st.button("Maybe"):
+        st.session_state.selected_suggestion = "Maybe"
 
 col7, col8, col9 = st.beta_columns(3)
 with col7:
-    st.button("Button 7")
+    if st.button("Try again"):
+        st.session_state.selected_suggestion = "Try again"
 with col8:
-    st.button("Button 8")
+    if st.button("Wait"):
+        st.session_state.selected_suggestion = "Wait"
 with col9:
-    st.button("Button 9")
+    if st.button("Think about it more"):
+        st.session_state.selected_suggestion = "Think about it more"
 
-# Define the text input at the bottom of the page
-st.text_input("Enter your decision here...")
+# Define the text input and send button
+if "selected_suggestion" in st.session_state:
+    st.text_input("Enter your decision here...", value=st.session_state.selected_suggestion)
+else:
+    text_input = st.text_input("Enter your decision here...")
+
+if st.button("Send"):
+    if "selected_suggestion" in st.session_state:
+        st.session_state.selected_suggestion = None
+        text_input = ""
+    else:
+        st.session_state.text_input = text_input
+        text_input = ""
